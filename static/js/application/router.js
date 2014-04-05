06c13cc21618616d2beee8d6f2d62aee781d2f0d
carlessAngeleno <daydream.han@gmail.com>
2014-04-05 14:15:44 -0700

    Add a new model, RecentSearches, to the SearchForm app
    References:
    http://stackoverflow.com/a/19466975
    http://stackoverflow.com/a/19466975

--- static/js/application/router.js
+++ static/js/application/router.js
@@ -4,7 +4,31 @@ SearchForm.Router.map(function() {
 });
 
 SearchForm.SearchFormRoute = Ember.Route.extend({
+  // http://stackoverflow.com/a/19466975
+  // http://stackoverflow.com/a/19466975
+  
   model: function() {
-    return this.store.find('timeframe');
+    return Em.RSVP.hash({
+    	timeframe: this.store.find('timeframe'),
+    	recentSearches: this.store.find('recentSearches')
+    });
+  },
+
+  setupController: function(controller, model) {
+    // You can use model.post to get post, etc
+    // Since the model is a plain object you can just use setProperties
+    controller.setProperties(model);
+    console.log(model);
   }
+
+// App.PostRoute = Em.Route.extend({
+//   model: function(params) {
+//     return Em.RSVP.hash({
+//       post:     // promise to get post
+//       comments: // promise to get comments,
+//       user:     // promise to get user
+//     });
+//   },
+
+
 });
