e9749b015e61807000b5cdd27e6c090777849fb8
carlessAngeleno <daydream.han@gmail.com>
2014-04-19 14:26:56 -0700

    When selecting one checkbox, de-select all other

--- static/js/application/controllers/timeframe_controller.js
+++ static/js/application/controllers/timeframe_controller.js
@@ -1,8 +1,11 @@
 SearchForm.TimeframeController = Ember.ObjectController.extend({
+  
+  needs: ['searchForm'],
 
   selected: function(key, value) {
+    
     var model = this.get('model');
-
+    
     if (value === undefined) {
       // getter
       return model.get('selected');
@@ -10,10 +13,22 @@ SearchForm.TimeframeController = Ember.ObjectController.extend({
       // setter
       model.set('selected', value);
       model.save();
+      
+      // de-select rest (only 1 active allowed)
+      var timeframes = this.get('controllers.searchForm.timeframe');
+      timeframes.forEach(
+        function(timeframe) {
+          if (timeframe.id !== this.id) {
+            timeframe.set('selected', false);
+            timeframe.save();
+          }
+        }, 
+        model
+      );
+
       return value;
     }
   }.property('model.selected')
 
 });
-
 
